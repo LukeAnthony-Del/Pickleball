f0ee1624edbc840a9a332fec56f7c423c78504a4
LukeAnthony-Del <Luke@dayandnight.co.za>
2026-09-16 16:47:15 +0100

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("button");
-const message = document.getElementById("message");
-
-button.addEventListener("click", function () {
-  message.textContent = "It works!";
-});
